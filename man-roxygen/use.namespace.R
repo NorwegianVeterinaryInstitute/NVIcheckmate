5ec02cacbb6f9cc177c32dc46172203206366de0
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-27 15:45:35 +0200

    doc: imported template use.namespace

---
+++ man-roxygen/use.namespace.R
@@ -0,0 +1,7 @@
+# File imported from https://gihub.com/mllg/checkmate/man-roxygen/ and modified for NVIcheckmate
+# Author and copyright Michel Lang, <michellang@@gmail.com>, see Licence and copyright.
+#' 
+#' @param use.namespace \[\code{logical(1)}\]\cr
+#'  Call functions of \pkg{checkmate} using its namespace explicitly.
+#'  Can be set to \code{FALSE} so save some microseconds, but the checkmate package needs to be imported.
+#'  Default is \code{TRUE}.
