eb8f99e39ab24fdcc151a8cc6f74ff6f9f67c90b
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-27 15:34:04 +0200

    doc: check_package updated help and examples. Some styling

--- R/check_package.R
+++ R/check_package.R
@@ -1,37 +1,53 @@
-#' @title Check if an argument is an installed package of required version 
-#' @description checks if an package is installed and if it is installed, check if the version is equal or higher than the required version.
+#' @title Check if an object is an installed package of the required version 
+#' @description Check if an package is installed and if it is installed, check if the version is equal 
+#'     to or higher than the required version.
 #'
 #' @templateVar fn package
 #' @template x
-#' @param version. The required version of the installed package. May be \code{NULL} 
+#' @param version \[character(1)\]\cr
+#'     The required version of the installed package. May be \code{NULL}.
 #' @template checker
 #' @export
 #' @examples
-#' TRUE 
+#' # returns TRUE 
 #' check_package(x = "checkmate" , version = "2.0.0")
-#' Error
+#' 
+#' # returns an error message
 #' check_package(x = "nopackage")
 
-check_package(x, version = NULL) {
-#ARGUMENT CHECKING 
-assert_character (x, len = 1, min.char = 2)
-assert_character(version, len = 1, null.ok = TRUE) 
-
+check_package <- function(x, version = NULL) {
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # Perform checks
+  checkmate::assert_character (x, len = 1, min.char = 2, add = checks)
+  checkmate::assert_character(version, len = 1, null.ok = TRUE, add = checks)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  # PERFORM CHECK
   # if the package is not installed
-  if (! nchar(system.file(package = x)))  {
-res <- paste0("The package '", x, "' is not installed") 
-} else {
-    # Check if the required version is  installed
-installed_version <- utils::packageDescription(x)$Version
-    if (utils::compareVersion(installed_version, version) == -1) {
-   res <- paste0("The package '", x, "' version '", installed_version, "' is  installed, while version '", version, "' is required." ) 
+  if (!nchar(system.file(package = x)))  {
+    res <- paste0("The package '", x, "' is not installed")
+  } else {
+    if (is.null(version)) { 
+      res <- TRUE
+    } else {
+      # Check if the required version is  installed
+      installed_version <- utils::packageDescription(x)$Version
+      if (utils::compareVersion(installed_version, version) == -1) {
+        res <- paste0("The package '", x, "' version '", installed_version, "' is installed, while version '", version, "' is required." )
       } else {
-res <- TRUE 
-} 
-} 
+        res <- TRUE
+      }
+    }
+  }
+  return(res)
+}
+
 
 #' @export
 #' @include makeAssertion.R
 #' @template assert
 #' @rdname check_package
-assert_package <- makeAssertionFunction(check_package, use.namespace = FALSE)
+assert_package <- checkmate::makeAssertionFunction(check_package, use.namespace = FALSE)
