6a3e7ade9e23c9cb6dc34329ff272a0d6c490311
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-27 15:36:39 +0200

    test: test_check_new_columns updated in accord with change in arguments to function

--- tests/testthat/test_check_new_columns.R
+++ tests/testthat/test_check_new_columns.R
@@ -5,35 +5,34 @@ library(testthat)
 context("check_new_columns")
 
 test_that("Make error for check_new_columns", {
-
-
+  
   expect_identical(
-  check_new_colname(columns = c("saksnr", "komnr"),
-                      new_column = "komnr",
+    check_new_colname(x = "komnr",
+                      columns = c("saksnr", "komnr"),
                       code_column = FALSE,
                       overwrite = FALSE),
-  paste("The column name(s): 'komnr' already exist in the data frame. Either give new column name(s) for the column(s)",
-        "named 'komnr' or specify overwrite = TRUE to replace values in the existing column(s) with new content."))
-
+    paste("The column name(s): 'komnr' already exist in the data frame. Either give new column name(s) for the column(s)",
+          "named 'komnr' or specify overwrite = TRUE to replace values in the existing column(s) with new content."))
+  
   expect_identical(
-  check_new_colname(columns = c("saksnr", "komnr"), new_column = "komnr", code_column = TRUE, overwrite = FALSE),
-  paste("The new column cannot have the same name as the code_column 'komnr`."))
-
+    check_new_colname(x = "komnr", columns = c("saksnr", "komnr"), code_column = TRUE, overwrite = FALSE),
+    paste("The new column cannot have the same name as the code_column 'komnr`."))
+  
   expect_identical(
-  check_new_colname(columns = c("saksnr", "komnr", "hensiktkode", "artkode", "dyreartkode"),
-                      new_column = c("hensiktkode", "artkode", "dyreartkode"),
+    check_new_colname(x = c("hensiktkode", "artkode", "dyreartkode"),
+                      columns = c("saksnr", "komnr", "hensiktkode", "artkode", "dyreartkode"),
                       code_column = FALSE, overwrite = FALSE),
-  paste("The column name(s): 'hensiktkode, artkode, dyreartkode' already exist in the data frame.",
-        "Either give new column name(s) for the column(s) named 'hensiktkode, artkode, dyreartkode' or specify",
-        "overwrite = TRUE to replace values in the existing column(s) with new content."))
-
+    paste("The column name(s): 'hensiktkode, artkode, dyreartkode' already exist in the data frame.",
+          "Either give new column name(s) for the column(s) named 'hensiktkode, artkode, dyreartkode' or specify",
+          "overwrite = TRUE to replace values in the existing column(s) with new content."))
+  
   expect_identical(
-  check_new_colname(columns = c("saksnr", "komnr", "hensiktkode", "artkode", "dyreartkode"),
-                      new_column = c("hensiktkode", "artkode", "dyreartkode"),
+    check_new_colname(x = c("hensiktkode", "artkode", "dyreartkode"),
+                      columns = c("saksnr", "komnr", "hensiktkode", "artkode", "dyreartkode"),
                       code_column = TRUE, overwrite = FALSE),
-  paste("The new column cannot have the same name as the code_column 'hensiktkode, artkode, dyreartkode`."))
-
-
+    paste("The new column cannot have the same name as the code_column 'hensiktkode, artkode, dyreartkode`."))
+  
+  
 })
 
 
