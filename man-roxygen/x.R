2b7e5620808a43d2b04a6f8e4746b1f8f1da55a4
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-27 15:43:20 +0200

    doc: templates updated or included x, fmatch, null.ok, assert Copyright statement added. Made as small changes as possible towards original Ensured that running without creating errors.

--- man-roxygen/x.R
+++ man-roxygen/x.R
@@ -1,2 +1,5 @@
-#' @param x [any]\cr
+# File imported from https://gihub.com/mllg/checkmate/man-roxygen/ and modified for NVIcheckmate
+# Author and copyright Michel Lang, <michellang@@gmail.com>, see Licence and copyright.
+#' 
+#' @param x \[any\]\cr
 #'  Object to check.
