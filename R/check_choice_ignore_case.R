967c7df456630507fbfd9d3fbaf6738fc64b0f97
Petter Hopp <Petter.Hopp@vetinst.no>
2021-05-23 10:52:06 +0200

    refactor: changed name to check_choice_ignore_case

--- R/check_choice_ignore_case.R
+++ R/check_choice_ignore_case.R
@@ -1,12 +1,12 @@
-#' @title Check if an object is an element of a given set regardless of case
-#' @description Check if an object is an element of a given set regardless of the object name is 
-#'     written in lower and upper case. The function is based on code/{checkmate::check_choice}.
+#' @title Check if an object is an element of a given set ignoring case
+#' @description Check if an object is an element of a given set ignoring case in 
+#'     the object name. The function is based on code/{checkmate::check_choice}.
 #' @details The object must be of type character. The check is intended for functions were using 
 #'     camelCase may make the argument easier to remember. Therefore, the input to the function is
-#'     made case insensitive.
+#'     ignoring case.
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #'
-#' @templateVar fn choice_anycase
+#' @templateVar fn choice_ignore_case
 #' @template x
 #' @param choices \[\code{character}\]\cr
 #'    Set of possible values.
@@ -16,16 +16,16 @@
 #'
 #' @examples
 #' # returns TRUE
-#' check_choice_anycase(x = "APPLE", 
+#' check_choice_ignore_case(x = "APPLE", 
 #'                      choices = c("Apple", "Pear", "Orange") , 
 #'                      null.ok = FALSE, 
 #'                      fmatch = FALSE)
 #' @export
 
-check_choice_anycase <- function(x, choices, null.ok = FALSE, fmatch = FALSE) {
+check_choice_ignore_case <- function(x, choices, null.ok = FALSE, fmatch = FALSE) {
   res <- checkmate::check_character(x=x, len = 1)
   if (res != TRUE) { return(res)}
-
+  
   xx <- tolower(x)
   choicesx <- tolower(choices)
   res <- checkmate::check_choice(x = xx, choices = choicesx , null.ok = null.ok, fmatch = fmatch)
@@ -34,7 +34,7 @@ check_choice_anycase <- function(x, choices, null.ok = FALSE, fmatch = FALSE) {
     res <- paste0(substr(res, 1, regexpr("\\{", res)[1]-1),
                   # set_collapse(choices),
                   paste0("{'", paste0(unique(choices), collapse = "','"), "'}"),
-                  " (case insensitive)",
+                  " (case is ignored)",
                   sub(xx, x, substr(res, regexpr("\\}", res)[1]+1, nchar(res))))
   }
   return(res)
@@ -45,5 +45,5 @@ check_choice_anycase <- function(x, choices, null.ok = FALSE, fmatch = FALSE) {
 #' @include makeAssertionFunction.R
 #' @template comment
 #' @template assert
-#' @rdname check_choice_anycase
-assert_choice_anycase  = makeAssertionFunction(check_choice_anycase) 
+#' @rdname check_choice_ignore_case
+assert_choice_ignore_case  = makeAssertionFunction(check_choice_ignore_case) 
