9c6b7e38fc6c4c4623ed67960341db56f4f1fba5
Petter Hopp <Petter.Hopp@vetinst.no>
2021-05-27 10:17:58 +0200

    refactor: check_package cleaned code for problems due to updating

--- R/check_package.R
+++ R/check_package.R
@@ -9,32 +9,22 @@
 #' @template checker
 #' @export
 #' @examples
-<<<<<<< HEAD
-#' TRUE i.e. no error message
-#' check_package(x = "checkmate" , version = "2.0.0")
-#' 
-#' Produces an error message
-=======
-#' # returns TRUE 
+#' # returns TRUE i.e. no error message
 #' check_package(x = "checkmate" , version = "2.0.0")
 #' 
 #' # returns an error message
->>>>>>> b18e74c479360dffccd115a46ca80a8aab4129be
 #' check_package(x = "nopackage")
 
 check_package <- function(x, version = NULL) {
   # ARGUMENT CHECKING ----
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
-  # Perform checks
-<<<<<<< HEAD
-  checkmate::assert_character (x, len = 1, min.char = 2)
-  checkmate::assert_character(version, len = 1, null.ok = TRUE)
-=======
+
+    # Perform checks
   checkmate::assert_character (x, len = 1, min.char = 2, add = checks)
   checkmate::assert_character(version, len = 1, null.ok = TRUE, add = checks)
->>>>>>> b18e74c479360dffccd115a46ca80a8aab4129be
-  # Report check-results
+
+    # Report check-results
   checkmate::reportAssertions(checks)
   
   # PERFORM CHECK
@@ -42,16 +32,6 @@ check_package <- function(x, version = NULL) {
   if (!nchar(system.file(package = x)))  {
     res <- paste0("The package '", x, "' is not installed")
   } else {
-<<<<<<< HEAD
-    # Check if the required version is  installed
-    installed_version <- utils::packageDescription(x)$Version
-    if (utils::compareVersion(installed_version, version) == -1) {
-      res <- paste0("The package '", x, "' version '", installed_version, "' is installed, while version '", version, "' is required." )
-    } else {
-      res <- TRUE
-    }
-  }
-=======
     if (is.null(version)) { 
       res <- TRUE
     } else {
@@ -65,7 +45,6 @@ check_package <- function(x, version = NULL) {
     }
   }
   return(res)
->>>>>>> b18e74c479360dffccd115a46ca80a8aab4129be
 }
 
 
