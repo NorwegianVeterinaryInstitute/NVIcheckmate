867926a1499388c9569a9b78078ec5bc9ae7d6b6
Petter Hopp <Petter.Hopp@vetinst.no>
2021-05-27 11:15:50 +0200

    doc: corrected links in doc for makeAssertionFunction

--- man-roxygen/makeFunction.R
+++ man-roxygen/makeFunction.R
@@ -5,7 +5,7 @@
 #'  Function which checks the input. Must return \code{TRUE} on success and a string with the error message otherwise.
 #' @param c.fun \[\code{character(1)}\]
 #'  If not \code{NULL}, instead of calling the function \code{check.fun}, use \code{.Call} to call a C function \dQuote{c.fun} with the identical
-#'  set of parameters. The C function must be registered as a native symbol, see \code{\link[base]{.Call}}.
+#'  set of parameters. The C function must be registered as a native symbol, see \code{\link[base]{CallExternal}}.
 #'  Useful if \code{check.fun} is just a simple wrapper.
 #' @param env \[\code{environment}\]
-#'  The environment of the created function. Default is the \code{\link[base]{parent.frame}}.
+#'  The environment of the created function. Default is the \code{parent.frame}, see \code{\link[base]{sys.parent}}.
