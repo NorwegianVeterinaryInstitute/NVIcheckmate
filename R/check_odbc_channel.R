361e2f7634b814271fe6fa5f5d58fcf2aa028624
Petter Hopp <Petter.Hopp@vetinst.no>
2021-07-01 08:15:23 +0200

    refactor: changed name to check_odbc_channel as more descriptive for function

--- R/check_odbc_channel.R
+++ R/check_odbc_channel.R
@@ -1,10 +1,10 @@
-#' @title Check if an object is an open RODBC-channel
-#' @description Check if an object is an open RODBC-channel.  
-#' @details The check uses \code{RODBC:::odbcValidChannel} to check if the argument is an open RODBC-channel.
+#' @title Check if an object is an open ODBC-channel
+#' @description Check if an object is an open ODBC-channel.  
+#' @details The check uses \code{RODBC:::odbcValidChannel} to check if the argument is an open ODBC-channel.
 #'     Be aware that this function is an internal \code{RODBC}-function and may change within that package without warning.
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #'
-#' @templateVar fn rodbc
+#' @templateVar fn odbc_channel
 #' @template x
 #' @param dbservice \[character(1)\]\cr
 #'     The database the channel should be connect to. 
@@ -18,11 +18,11 @@
 #' journal_rapp <- login("PJS")
 #'
 #' # error check:
-#' check_rodbc(x = "journal_rapp", dbservice = "PJS")
+#' check_odbc_channel(x = "journal_rapp", dbservice = "PJS")
 #' }
 
 
-check_rodbc <- function(x, dbservice = NULL) {
+check_odbc_channel <- function(x, dbservice = NULL) {
   if(!RODBC:::odbcValidChannel(x)) {
     res <- paste(deparse(substitute(x)),
                   "is not an open RODBC channel")
@@ -37,5 +37,5 @@ check_rodbc <- function(x, dbservice = NULL) {
 #' @include makeAssertionFunction.R
 #' @template comment
 #' @template assert
-#' @rdname check_rodbc
-assert_rodbc = makeAssertionFunction(check_rodbc)
+#' @rdname check_odbc_channel
+assert_odbc_channel = makeAssertionFunction(check_odbc_channel)
