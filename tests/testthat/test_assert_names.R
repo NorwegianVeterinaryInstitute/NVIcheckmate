e93886fe69ffb0a6608498c93705f293ddcbe832
Petter Hopp <Petter.Hopp@vetinst.no>
2021-07-16 09:23:18 +0200

    test: included test_assert_names

---
+++ tests/testthat/test_assert_names.R
@@ -0,0 +1,30 @@
+library(NVIcheckmate)
+library(testthat)
+# library(checkmate)
+context("assert_names")
+
+test_that("No error for assert_names", {
+  
+  expect_identical(
+    assert_names(x = "Tomato",
+                               type = "named",
+                               disjunct.from = c("Apple", "Pear", "Orange", "Banana")),
+    "Tomato")
+  
+  expect_identical(
+    assert_names(x = "Apple",
+                               type = "named",
+                               identical.to = c("Apple")),
+    "Apple")
+} )
+
+test_that("Make error for assert_names", {
+  
+  expect_error(
+    assert_names(x = "Apple",
+                               type = "named",
+                               disjunct.from = c("Apple", "Pear", "Orange", "Banana"),
+                               comment = "Please choose another name"),
+    regexp = "failed: Must be disjunct from")
+
+})
