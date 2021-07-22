3a156e0596d5326f4f29606894805be90d07c78a
Petter Hopp <Petter.Hopp@vetinst.no>
2021-07-22 13:06:21 +0200

    test: test_assert_names includes test for correct adding of comment.

--- tests/testthat/test_assert_names.R
+++ tests/testthat/test_assert_names.R
@@ -7,14 +7,14 @@ test_that("No error for assert_names", {
   
   expect_identical(
     assert_names(x = "Tomato",
-                               type = "named",
-                               disjunct.from = c("Apple", "Pear", "Orange", "Banana")),
+                 type = "named",
+                 disjunct.from = c("Apple", "Pear", "Orange", "Banana")),
     "Tomato")
   
   expect_identical(
     assert_names(x = "Apple",
-                               type = "named",
-                               identical.to = c("Apple")),
+                 type = "named",
+                 identical.to = c("Apple")),
     "Apple")
 } )
 
@@ -22,9 +22,9 @@ test_that("Make error for assert_names", {
   
   expect_error(
     assert_names(x = "Apple",
-                               type = "named",
-                               disjunct.from = c("Apple", "Pear", "Orange", "Banana"),
-                               comment = "Please choose another name"),
-    regexp = "failed: Must be disjunct from")
-
+                 type = "named",
+                 disjunct.from = c("Apple", "Pear", "Orange", "Banana"),
+                 comment = "Please choose another name"),
+    regexp = "Must be disjunct from \\(Apple,Pear,Orange,Banana). Please choose another name.")
+  
 })
