b8a8fac372bb44faf6f9bf2c073fd8d883259882
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-07-29 12:58:42 +0200

    style: x.R updated copyright

--- man-roxygen/x.R
+++ man-roxygen/x.R
@@ -1,5 +1,11 @@
-# File imported from https://gihub.com/mllg/checkmate/man-roxygen/ and modified for NVIcheckmate
-# Author and copyright Michel Lang, <michellang@@gmail.com>, see Licence and copyright.
+# BSD 3-Clause License
+# 
+# Copyright (c) 2019, Michel Lang
+# All rights reserved.
+# 
+# The file has been imported from https://gihub.com/mllg/checkmate/man-roxygen/ 
+# Modifications
+# - Square brackets are escaped
 #' 
 #' @param x \[any\]\cr
 #'  Object to check.
