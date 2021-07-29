5cf5ec7d18844d39f2f5e0b2efff7ddd9ae0af25
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-07-29 12:53:30 +0200

    style: fmatch.R updated copyright

--- man-roxygen/fmatch.R
+++ man-roxygen/fmatch.R
@@ -1,5 +1,11 @@
-# File imported from https://gihub.com/mllg/checkmate/man-roxygen/ and modified for NVIcheckmate
-# Author and copyright Michel Lang, <michellang@@gmail.com>, see Licence and copyright.
+# BSD 3-Clause License
+# 
+# Copyright (c) 2019, Michel Lang
+# All rights reserved.
+# 
+# The file has been imported from https://gihub.com/mllg/checkmate/man-roxygen/ 
+# Modifications
+# - Square brackets are escaped
 #' 
 #' @param fmatch \[\code{logical(1)}\]\cr
 #'     Use the set operations implemented in \code{fmatch} in package \pkg{fastmatch}.
@@ -7,9 +13,3 @@
 #'     \code{fastmatch::fmatch} modifies \code{y} by reference:
 #'     A hash table is added as attribute which is used in subsequent calls.
 #'  
-# Original text
-# #' @param fmatch [\code{logical(1)}]\cr
-# #'  Use the set operations implemented in \code{\link[fastmatch]{fmatch}} in package \pkg{fastmatch}.
-# #'  If \pkg{fastmatch} is not installed, this silently falls back to \code{\link[base]{match}}.
-# #'  \code{\link[fastmatch]{fmatch}} modifies \code{y} by reference:
-# #'  A hash table is added as attribute which is used in subsequent calls.
