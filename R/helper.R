364aea5c6cf634265315c3a19724181e7b1a51d7
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-07-29 13:39:51 +0200

    chore: helper.R updated with copyright

--- R/helper.R
+++ R/helper.R
@@ -1,3 +1,12 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2019, Michel Lang
+# All rights reserved.
+#
+# The file has been imported from https://gihub.com/mllg/checkmate/R/ 
+# Modifications
+# - Commented out all functions except mstop
+# 
 mstop = function(msg, ..., call. = NULL) {
   stop(simpleError(sprintf(msg, ...), call.))
 }
