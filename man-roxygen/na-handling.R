dbc3f5bc0d317524e6fc37c19a7e903fca23a5ef
Petter Hopp <Petter.Hopp@vetinst.no>
2021-09-05 19:16:20 +0200

    doc: imported template "na-handling.R" from checkmate

---
+++ man-roxygen/na-handling.R
@@ -0,0 +1,12 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2019, Michel Lang
+# All rights reserved.
+# 
+# The file has been imported from https://gihub.com/mllg/checkmate/man-roxygen/ 
+# Modifications
+# - none
+#' 
+#' @details This function does not distinguish between
+#' \code{NA}, \code{NA_integer_}, \code{NA_real_}, \code{NA_complex_}
+#' \code{NA_character_} and \code{NaN}.
