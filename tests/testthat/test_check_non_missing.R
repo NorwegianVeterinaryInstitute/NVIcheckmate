f42e64433a5e95f137ad9457d60656844cc34bc0
Petter Hopp <Petter.Hopp@vetinst.no>
2021-09-14 08:19:27 +0200

    test: fixed test_check_non_missing

--- tests/testthat/test_check_non_missing.R
+++ tests/testthat/test_check_non_missing.R
@@ -1,55 +1,54 @@
-test_check_non_missing
 library(NVIcheckmate)
 library(testthat)
 library(checkmate)
 
 test_that("No error for check_non_missing", {
-fruit <- "Apple" 
-vegetable <- NULL
+  fruit <- "Apple" 
+  vegetable <- NULL
   expect_identical(
-    check_non_missing( x = c("fruit", "vegetables"),
+    check_non_missing( x = list(fruit, vegetable)),
     TRUE)
   
   fruit <- "Apple" 
-vegetable <- NA
+  vegetable <- NA
   expect_identical(
-    check_non_missing( x = c("fruit", "vegetables"),
+    check_non_missing( x = list(fruit, vegetable)),
     TRUE)
-
+  
   fruit <- NA
-vegetable <- NULL
-cereal = "barley" 
+  vegetable <- NULL
+  cereal = "barley" 
   expect_identical(
-    check_non_missing( x = c("fruit", "vegetables", cereal),
+    check_non_missing( x = list(fruit, vegetable, cereal)),
     TRUE)
   
 } )
 
 test_that("Make error for check_non_missing", {
   fruit <- NULL
-vegetable <- NULL
+  vegetable <- NULL
   expect_error(
-    check_non_missing( x = c("fruit", "vegetables"),
-  regexp = "At least one of the arguments" )
+    check_non_missing( x = list(fruit, vegetable)),
+    regexp = "At least one of the arguments" )
   
-fruit <- NA
-vegetable <- NULL
-  expect_error(
-    check_non_missing( x = c("fruit", "vegetables"),
-  regexp = "At least one of the arguments" )
-
   fruit <- NA
-vegetable <- NA
-cereal = NA
+  vegetable <- NULL
   expect_error(
-    check_non_missing( x = c("fruit", "vegetables", "cereal"),
-  regexp = "At least one of the arguments" )
+    check_non_missing( x = list(fruit, vegetable)),
+    regexp = "At least one of the arguments" )
   
-fruit <- NULL
-vegetable <- NA
-cereal = NULL 
+  fruit <- NA
+  vegetable <- NA
+  cereal = NA
   expect_error(
-    check_non_missing( x = c("fruit", "vegetables", "cereal"),
-  regexp = "At least one of the arguments" )
-
+    check_non_missing( x = list(fruit, vegetable, cereal)),
+    regexp = "At least one of the arguments" )
+  
+  fruit <- NULL
+  vegetable <- NA
+  cereal = NULL 
+  expect_error(
+    check_non_missing( x = list(fruit, vegetable, cereal)),
+    regexp = "At least one of the arguments" )
+  
 })
