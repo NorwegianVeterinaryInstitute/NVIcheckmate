d4a63726671cbafe99d945aad94dc59bb389cfe0
Petter Hopp <Petter.Hopp@vetinst.no>
2021-09-15 08:08:52 +0200

    style: removed spaces in test_check_non_missing

--- tests/testthat/test_check_non_missing.R
+++ tests/testthat/test_check_non_missing.R
@@ -1,25 +1,25 @@
-library(NVIcheckmate)
 library(testthat)
 library(checkmate)
+library(NVIcheckmate)
 
 test_that("No error for check_non_missing", {
   fruit <- "Apple" 
   vegetable <- NULL
   expect_identical(
-    check_non_missing( x = list(fruit, vegetable)),
+    check_non_missing(x = list(fruit, vegetable)),
     TRUE)
   
   fruit <- "Apple" 
   vegetable <- NA
   expect_identical(
-    check_non_missing( x = list(fruit, vegetable)),
+    check_non_missing(x = list(fruit, vegetable)),
     TRUE)
   
   fruit <- NA
   vegetable <- NULL
   cereal = "barley" 
   expect_identical(
-    check_non_missing( x = list(fruit, vegetable, cereal)),
+    check_non_missing(x = list(fruit, vegetable, cereal)),
     TRUE)
   
 } )
@@ -28,27 +28,27 @@ test_that("Make error for check_non_missing", {
   fruit <- NULL
   vegetable <- NULL
   expect_error(
-    check_non_missing( x = list(fruit, vegetable)),
+    assert_non_missing(x = list(fruit, vegetable)),
     regexp = "At least one of the arguments" )
   
   fruit <- NA
   vegetable <- NULL
   expect_error(
-    check_non_missing( x = list(fruit, vegetable)),
+    assert_non_missing(x = list(fruit, vegetable)),
     regexp = "At least one of the arguments" )
   
   fruit <- NA
   vegetable <- NA
   cereal = NA
   expect_error(
-    check_non_missing( x = list(fruit, vegetable, cereal)),
+    assert_non_missing(x = list(fruit, vegetable, cereal)),
     regexp = "At least one of the arguments" )
   
   fruit <- NULL
   vegetable <- NA
   cereal = NULL 
   expect_error(
-    check_non_missing( x = list(fruit, vegetable, cereal)),
+    assert_non_missing(x = list(fruit, vegetable, cereal)),
     regexp = "At least one of the arguments" )
   
 })
