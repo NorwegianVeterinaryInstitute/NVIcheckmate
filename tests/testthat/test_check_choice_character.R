5178dd0c3f2f9ebb1179f845ca28947067de1305
Petter Hopp <petter.hopp@vetinst.no>
2022-03-16 17:13:09 +0100

    refactor: test_check_choice_character with standard line width when error checking

--- tests/testthat/test_check_choice_character.R
+++ tests/testthat/test_check_choice_character.R
@@ -1,7 +1,6 @@
 library(NVIcheckmate)
 library(testthat)
 library(checkmate)
-context("check_choice_character")
 
 test_that("No error for check_choice_character", {
   
@@ -15,6 +14,9 @@ test_that("No error for check_choice_character", {
 
 
 test_that("Make error for check_choice_character", {
+  linewidth <- options("width")
+  options(width = 80)
+  
   expect_identical(
     check_choice_character(x = "Apples",
                            choices = c("Apple", "Pear", "Orange", "Pineapple"),
@@ -27,5 +29,5 @@ test_that("Make error for check_choice_character", {
                            ignore.case = TRUE),
     "Must have length 1, but has length 2" )
   
-  
+  options(width = unlist(linewidth))
 })
