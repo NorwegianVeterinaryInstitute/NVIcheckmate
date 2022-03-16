9c8268cb83819b73146349a8ad8a2572d9d37f10
Petter Hopp <petter.hopp@vetinst.no>
2022-03-16 17:12:11 +0100

    refactor: test_check_subset_character error checking changed to using expect_error

--- tests/testthat/test_check_subset_character.R
+++ tests/testthat/test_check_subset_character.R
@@ -24,15 +24,17 @@ test_that("No error for check_subset_character", {
 } )
 
 test_that("Make error for check_subset_character", {
-  expect_identical(
-    check_subset_character(x = "Tomato",
+  expect_error(
+    assert_subset_character(x = "Tomato",
                            choices = c("Apple", "Pear", "Orange", "Banana"),
                            ignore.case = TRUE),
-    "Must be a subset of {'Apple', 'Pear', 'Orange', 'Banana'} (case is ignored), but is {'Tomato'}")
+    regexp = "Must be a subset of {'Apple', 'Pear', 'Orange', 'Banana'} (case is ignored), but is {'Tomato'}",
+    fixed = TRUE)
   
-  expect_identical(
-    check_subset_character(x = c("Tomato", "Apple"), 
+  expect_error(
+    assert_subset_character(x = c("Tomato", "Apple"), 
                            choices = c("Apple", "Pear", "Orange", "Banana"),
                            ignore.case = TRUE),
-    "Must be a subset of {'Apple', 'Pear', 'Orange', 'Banana'} (case is ignored), but is {'Tomato', 'Apple'}")
+    regexp = "Must be a subset of {'Apple', 'Pear', 'Orange', 'Banana'} (case is ignored), but is {'Tomato', 'Apple'}",
+    fixed = TRUE)
 })
