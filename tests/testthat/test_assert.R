04f1cd11ac367410c55bcc793858d8d024d29879
Petter Hopp <petter.hopp@vetinst.no>
2022-03-16 17:14:24 +0100

    refactor: test_assert with standard line width when error checking

--- tests/testthat/test_assert.R
+++ tests/testthat/test_assert.R
@@ -5,23 +5,29 @@ context("assert")
 
 test_that("Include comment in assert", {
   
-x <- "Tomato"
-  expect_error(
-assert(checkmate::check_choice(x, choices = c("Apple", "Pear", "Orange", "Banana")),
-       checkmate::check_data_frame(x), 
-       combine = "or",
-       comment = "Something was wrong"),
-    regexp = " not 'character'.\\nSomething was wrong")
+  linewidth <- options("width")
+  options(width = 80)
   
-
-x <- "Tomato"
-checks <- checkmate::makeAssertCollection()
-assert(checkmate::check_choice(x, choices = c("Apple", "Pear", "Orange", "Banana")),
-       checkmate::check_data_frame(x), 
-       combine = "and",
-       comment = "Something was wrong",
-       add = checks)
-expect_identical(grep("Something was wrong",checks$getMessages()), c(1L,2L))
-
+  x <- "Tomato"
+  expect_error(
+    assert(checkmate::check_choice(x, choices = c("Apple", "Pear", "Orange", "Banana")),
+           checkmate::check_data_frame(x), 
+           combine = "or",
+           comment = "Something was wrong"),
+    regexp = "not\n * 'character'.\nSomething was wrong",
+    fixed = TRUE)
+  
+  
+  x <- "Tomato"
+  checks <- checkmate::makeAssertCollection()
+  assert(checkmate::check_choice(x, choices = c("Apple", "Pear", "Orange", "Banana")),
+         checkmate::check_data_frame(x), 
+         combine = "and",
+         comment = "Something was wrong",
+         add = checks)
+  expect_identical(grep("Something was wrong",checks$getMessages()), c(1L,2L))
+  
+  options(width = unlist(linewidth))
+  
 } )
 
