bbc405744b3f02191f4add96ce86ea2ad2f68bea
Petter Hopp <petter.hopp@vetinst.no>
2022-10-11 16:01:09 +0200

    style: styled spaces in test_assert_disjunct

--- tests/testthat/test_assert_disjunct.R
+++ tests/testthat/test_assert_disjunct.R
@@ -2,12 +2,12 @@ library(NVIcheckmate)
 library(testthat)
 
 test_that("No error for assert_disjunct", {
-  
+
   expect_identical(
     assert_disjunct(x = "Tomato",
                     y = c("Apple", "Pear", "Orange", "Banana")),
     "Tomato")
-  
+
   expect_identical(
     assert_disjunct(x = "Apple",
                     y = c("apple")),
@@ -15,11 +15,11 @@ test_that("No error for assert_disjunct", {
 })
 
 test_that("Make error for assert_disjunct", {
-  
+
   expect_error(
     assert_disjunct(x = "Apple",
                     y = c("Apple", "Pear", "Orange", "Banana"),
                     comment = "Please choose another name"),
     regexp = "be disjunct from \\{'Apple','Pear','Orange','Banana'},")
-  
+
 })
