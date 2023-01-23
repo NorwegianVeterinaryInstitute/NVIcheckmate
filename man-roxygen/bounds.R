d263a79c3bbb259695544e91ab649fb2439ab98e
Petter Hopp <Petter.Hopp@vetinst.no>
2023-01-23 09:31:48 +0100

    perf: imported man-roxygen/bounds.R from checkmate

---
+++ man-roxygen/bounds.R
@@ -0,0 +1,13 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2022, Michel Lang
+# All rights reserved.
+# 
+# The file has been imported from https://gihub.com/mllg/checkmate/man-roxygen/ 
+# Modifications
+# - Square brackets are escaped
+#' 
+#' @param lower \[\code{numeric(1)}\]\cr
+#'  Lower value all elements of \code{x} must be greater than or equal to.
+#' @param upper \[\code{numeric(1)}\]\cr
+#'  Upper value all elements of \code{x} must be lower than or equal to.
