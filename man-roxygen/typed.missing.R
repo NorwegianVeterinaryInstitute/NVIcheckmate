bc86d28cefa7459e39f8d77da8df3bb3e2c57553
Petter Hopp <Petter.Hopp@vetinst.no>
2023-01-23 09:32:33 +0100

    perf: imported man-roxygen/typed.missing.R from checkmate Used by assert_integer

---
+++ man-roxygen/typed.missing.R
@@ -0,0 +1,14 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2022, Michel Lang
+# All rights reserved.
+# 
+# The file has been imported from https://gihub.com/mllg/checkmate/man-roxygen/ 
+# Modifications
+# - Square brackets are escaped
+#' 
+#' @param typed.missing \[\code{logical(1)}\]\cr
+#'  If set to \code{FALSE} (default), all types of missing values (\code{NA}, \code{NA_integer_},
+#'  \code{NA_real_}, \code{NA_character_} or \code{NA_character_}) as well as empty vectors are allowed
+#'  while type-checking atomic input.
+#'  Set to \code{TRUE} to enable strict type checking.
