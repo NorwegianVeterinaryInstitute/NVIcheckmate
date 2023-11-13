cba81ce9ba3a1d61deebd7a63ae6935bd9652af2
Petter Hopp <Petter.Hopp@vetinst.no>
2023-11-13 07:45:58 +0100

    test: styled test_assert_integerish

--- tests/testthat/test_assert_integerish.R
+++ tests/testthat/test_assert_integerish.R
@@ -1,24 +1,23 @@
-library(NVIcheckmate) 
+library(NVIcheckmate)
 library(testthat)
 test_that("No error for assert_integerish", {
   expect_identical(
     assert_integerish(x = 1),
-    1)	
-  
+    1)
+
   expect_identical(
-    assert_integerish(x = c(0, 1, 2)), 
+    assert_integerish(x = c(0, 1, 2)),
     c(0, 1, 2))
 })
-  
-  test_that("Make error for assert_integerish", { 
-    linewidth <- options("width") 
+
+  test_that("Make error for assert_integerish", {
+    linewidth <- options("width")
     options(width = 80)
-    
+
   expect_error(
-    assert_integerish(x = 1990, lower = 1995, 
-                      comment = "The year must be at least 1995"), 
+    assert_integerish(x = 1990, lower = 1995,
+                      comment = "The year must be at least 1995"),
     regexp = "The year must be at least 1995")
-  
+
   options(width = unlist(linewidth))
   })
-  
