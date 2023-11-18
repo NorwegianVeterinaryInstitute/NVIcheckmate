7d803ef2b6dd3b3d49f40348c71109389bfd588b
Petter Hopp <Petter.Hopp@vetinst.no>
2023-11-18 14:08:48 +0100

    test: created more tests in test_assert_character

--- tests/testthat/test_assert_character.R
+++ tests/testthat/test_assert_character.R
@@ -5,12 +5,19 @@ test_that("No error for assert_character", {
 
   expect_identical(
     assert_character(x = "Tomato",
-                min.chars = 1, max.chars = 20, any.missing = FALSE, len = 1),
+                     min.chars = 1, max.chars = 20, any.missing = FALSE, len = 1),
     "Tomato")
+  
+  expect_identical(
+    assert_character(x = c("Tomato", "Potato"),
+                     min.chars = 1, max.chars = 20, any.missing = FALSE, max.len = 4),
+    c("Tomato", "Potato"))
 })
 
 test_that("Make error for assert_character", {
-
+  linewidth <- options("width")
+  options(width = 80)
+  
   expect_error(
     assert_character(x = "Tomato",
                      min.chars = 1, max.chars = 5, any.missing = FALSE, len = 1),
@@ -22,4 +29,16 @@ test_that("Make error for assert_character", {
                      comment = "Vegetables must have at most 5 characters"),
     regexp = "Vegetables must have at most 5 characters")
 
+  expect_error(
+    assert_character(x = c("Tomato", "Potato"),
+                     min.chars = 1, max.chars = 20, any.missing = FALSE, len = 1),
+    regexp = "Must have length 1, but has length 2")
+  
+  expect_error(
+    assert_character(x = c("Tomato", "Potato"),
+                     min.chars = 1, max.chars = 20, any.missing = FALSE, max.len = 4,
+                     ignore.case = TRUE, pattern = "mato"),
+    regexp = "Must comply to pattern 'mato'")
+  
+  options(width = unlist(linewidth))
 })
